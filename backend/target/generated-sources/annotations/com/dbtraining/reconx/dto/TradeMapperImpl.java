92688344a805ad3a7128173595f214c71157c5bb
Aryan <aryan.singhal613@gmail.com>
2026-08-04 15:55:43 +0530

    improvements

--- backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
+++ backend/target/generated-sources/annotations/com/dbtraining/reconx/dto/TradeMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-08-04T11:12:24+0530",
-    comments = "version: 1.6.2, compiler: javac, environment: Java 25.0.3 (Microsoft)"
+    date = "2026-08-04T14:43:51+0530",
+    comments = "version: 1.6.2, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
 )
 @Component
 public class TradeMapperImpl implements TradeMapper {
